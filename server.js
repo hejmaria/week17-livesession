47daaf8fa9f31b56427791776b7fd1a7a22d289f
Jennie Dalgren <jennie.dalgren@gmail.com>
2021-12-06 11:38:55 +0100

    monday live session code

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
-// Defines the port the app will run on. Defaults to 8080, but can be
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+import techFundings from './data/tech_fundings.json'
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -13,12 +11,42 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
+const users = [
+  { id: 1, name: 'Alice', age: 33 },
+  { id: 2, name: 'Bob', age: 23 },
+  { id: 3, name: 'Chris', age: 3 },
+  { id: 4, name: 'Daniela', age: 67 },
+]
+
+// This is our first endpoint
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello from us!')
+})
+
+// get a list of users
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+// get a list of the companies with fundings (from json file)
+app.get('/fundings', (req, res) => {
+  res.json(techFundings)
+})
+
+// get a specific company based on id, using param
+app.get('/fundings/:id', (req, res) => {
+  const { id } = req.params
+
+  const companyId = techFundings.find(company => company.index === +id)
+
+  if (!companyId) {
+    res.status(404).send('No company found with that id')
+  } else {
+    res.json(companyId)
+  }
 })
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
+  console.log(`Server running on http://localhost:${port} YAY YAY`)
 })
